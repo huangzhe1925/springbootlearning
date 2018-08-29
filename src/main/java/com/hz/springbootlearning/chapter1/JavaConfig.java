b26c7dbea2333249cd735438c67a14eb6bd59587
Administrator <Administrator@VZHAFG99YCB3AGX>
2018-08-29 20:18:27 +0800

    add java config

---
+++ src/main/java/com/hz/springbootlearning/chapter1/JavaConfig.java
@@ -0,0 +1,18 @@
+package com.hz.springbootlearning.chapter1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+
+	@Bean
+	public FunctionService functionService(){
+		return new FunctionService();
+	}
+	
+	@Bean
+	public UseFunctionService useFunctionService(){
+		return new UseFunctionService();
+	}
+}
--- src/main/java/com/hz/springbootlearning/chapter1/Main.java
+++ src/main/java/com/hz/springbootlearning/chapter1/Main.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 	public static void main(String args[]) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
+//		UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
 		UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
 		System.out.println(useFunctionService.sayHello(" World"));
+		
 		context.close();
 	}
 }
